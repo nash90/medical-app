64fb8c78c645d44936d22df7fc6d462d13e3e2e0
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-07-17 11:33:13 -0700

    impleted the function to upload the drug_class to database

--- script/upload_data.py
+++ script/upload_data.py
@@ -6,7 +6,10 @@ import pandas as pd
 
 from base import Base
 from base import engine
+from base import Session
 from config import settings
+
+from models import DrugClass
 
 # set constants colomn names in excel
 Drug_Class = "Drug_Class"
@@ -17,25 +20,34 @@ Drug_Information_Type = "Drug_Information_Type"
 Drug_Information = "Drug_Information"
 Scrabble_Hint = "Scrabble_Hint"
 Keyword = "Keyword"
+Quiz_Question = "Quiz_Question"
+Quiz_Type = "Quiz_Type"
+Enable = "Enable"
 
 
 file = settings['file_src']
 drug_info_sheet_name = settings['drug_info_sheet_name']
 
-df_drug_info = pd.read_excel(file, sheetname=drug_info_sheet_name)
+df_drug_info = pd.read_excel(file, sheet_name=drug_info_sheet_name)
 #print(df_drug_info)
 
 def createTableIfNotExit():
   Base.metadata.create_all(engine)
 
 def updateDrugClass(row):
-  drug_class = row["Drug_Class"]
+  drug_class = row[Drug_Class]
+  if type(drug_class) == str:
+    has_class = session.query(DrugClass).filter(DrugClass.drug_class_name == drug_class).all()
+    if len(has_class) < 1:
+      new_drug_class = DrugClass(drug_class_name=drug_class)
+      session.add(new_drug_class)
 
 def run_script():
-  #createTableIfNotExit()
   for index, row in df_drug_info.iterrows():
-    updateDrugClass() 
-#session = Session()
+    updateDrugClass(row) 
 
-#session.commit()
-#session.close()
+session = Session()
+createTableIfNotExit()
+run_script()
+session.commit()
+session.close()
