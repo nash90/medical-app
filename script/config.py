cd3b3847b588179ff5a1a09807a011950382fd04
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-07-17 11:04:20 -0700

    create test database and user

--- script/config.py
+++ script/config.py
@@ -3,8 +3,8 @@ import os
 settings = {
   "db_type":"postgresql",
   "db_url":"localhost:5432/medicaldb",
-  "db_username":"postgres",
-  "db_password":"",
+  "db_username":"test_user",
+  "db_password":"test1234",
   "file_src":"/Users/abinash/Desktop/project/healthapp/data/Cardio and Mental Health_ IPPE project.xlsx",
   "drug_info_sheet_name":"Project",
   "quiz_sheet_name":"Drug Quiz Question",
