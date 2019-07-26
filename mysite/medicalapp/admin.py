d9ee2415e72b6896dbb6c44ba88b6191d90ff2a4
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-07-26 10:55:27 -0700

    created django models for DrugClass and DrugSubClass and plugin to admin dashboard

--- mysite/medicalapp/admin.py
+++ mysite/medicalapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import DrugClass
+from .models import DrugSubClass
+
+admin.site.register(DrugClass)
+admin.site.register(DrugSubClass)
--- mysite/medicalapp/models.py
+++ mysite/medicalapp/models.py
@@ -1,3 +1,25 @@
+import os
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class DrugClass(models.Model):
+  class Meta:
+    db_table = "drug_class"
+  drug_class_id = models.AutoField(primary_key=True)
+  drug_class_name = models.CharField(max_length=50)
+  drug_class_description = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.drug_class_name
+
+class DrugSubClass(models.Model):
+  class Meta:
+    db_table = "drug_subclass"  
+  drug_subclass_id = models.AutoField(primary_key=True)
+  drug_class = models.ForeignKey(DrugClass, null=True, on_delete=models.SET_NULL)
+  drug_subclass_name = models.CharField(max_length=50)
+  drug_subclass_description = models.CharField(max_length=200)
+
+  def __str__(self):
+      return self.drug_subclass_name
+
