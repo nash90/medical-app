174c706af4077cf5586d06372314f13287c2c178
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-07-26 10:23:17 -0700

    connected django to postgresql database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PSQL_DB_NAME'],
+        'USER': os.environ['PSQL_DB_USER'],
+        'PASSWORD': os.environ['PSQL_DB_PWD'],
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
