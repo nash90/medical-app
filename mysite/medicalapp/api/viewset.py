8b60a8f38b7675ae9e54bd9f90031803d0eab4c7
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-08-01 18:00:45 -0700

    seperated permission logic

--- mysite/medicalapp/api/viewset.py
+++ mysite/medicalapp/api/viewset.py
@@ -3,25 +3,9 @@ from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import renderers
-from django.core import serializers
-from django.http import HttpResponse, JsonResponse
 
 from ..models import Drug
 from .serializer import DrugSerializer
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.owner == request.user
 
 class DrugViewSet(viewsets.ModelViewSet):
     queryset = Drug.objects.all()
