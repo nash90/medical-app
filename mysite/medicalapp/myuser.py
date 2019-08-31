0fdc685782c89a1084861523071d35755ff1ed68
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-08-30 17:48:06 -0700

    call user model using function

--- mysite/medicalapp/myuser.py
+++ mysite/medicalapp/myuser.py
@@ -2,11 +2,11 @@ import os
 from django.db import models
 
 from django.contrib import auth
-from .models import MyUser
+User = auth.get_user_model()
 
 class Profile(models.Model):
   class Meta:
     db_table = 'profile'
   profile_id = models.AutoField(primary_key=True)
-  user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   date_of_birth = models.DateField()
