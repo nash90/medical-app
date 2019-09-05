862c576b3cacbc476fb03ee9d6575afa99c8e0ca
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-09-05 10:35:12 -0700

    create db if not exist

--- script/base.py
+++ script/base.py
@@ -3,12 +3,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exist
 from config import settings
 
 url = settings["db_url"]
 db_type = settings["db_type"]
 username = settings["db_username"]
 password = settings["db_password"]
-engine = create_engine(db_type+'://'+username+':'+password + '@'+ url)
+full_url = db_type+'://'+username+':'+password + '@'+ url
+if not database_exists(url):
+    create_database(url)
+engine = create_engine(full_url)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
