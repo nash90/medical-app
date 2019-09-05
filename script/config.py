c49726b4520baf38b29431324f8b270fdfa52936
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-09-05 10:15:39 -0700

    provide data file in script folder

--- script/config.py
+++ script/config.py
@@ -5,7 +5,7 @@ settings = {
   "db_url":"localhost:5432/medicaldb",
   "db_username":"test_user",
   "db_password":"test1234",
-  "file_src":"/Users/abinash/Desktop/project/healthapp/data/Cardio and Mental Drug Game.xlsx",
+  "file_src":"./data/Cardio and Mental Drug Game.xlsx",
   "drug_info_sheet_name":"Project",
   "quiz_sheet_name":"Drug Quiz Question",
   "quiz_option_sheet_name":"Drug Quiz Option",
