30926dc8124ec0edcc7f82aedecafcb945a04420
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-12-17 21:14:01 -0800

    homepage to static webpage

--- mysite/medicalapp/views.py
+++ mysite/medicalapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("MedScrab is currently under construction…. Please check back later!")
+    homepage_url = settings.STATIC_WEBSITE_URL
+    #return HttpResponse("MedScrab is currently under construction…. Please check back later!")
+    return HttpResponseRedirect(homepage_url)
